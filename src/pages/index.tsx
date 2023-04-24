72d7c61607cb40d1cd5a18ad9e7887fe22942ca1
Jaxon Scott <jaxonscott@Jaxons-MacBook-Pro.local>
2023-04-24 18:19:33 -0400

    added timer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,34 @@
 import { type NextPage } from "next";
+import React from "react";
 import Head from "next/head";
 import { useState, useEffect } from "react";
 import clsx from "clsx";
+import Countdown from "react-countdown";
 
 const Home: NextPage = () => {
   const [correct, setCorrect] = useState(true);
+  const [started, setStarted] = useState(false);
   const [index, setIndex] = useState(0);
   const [pressedKeys, setPressedKeys] = useState([]);
 
+  const handleTimerState = () => {
+    setStarted(true);
+  };
+  //timer
+  const time = React.useMemo(() => {
+    return Date.now() + 30 * 1000;
+  }, [started]);
+
+  const renderer = ({ seconds }: { seconds: number }) => {
+    if (!started) {
+      return <button onClick={handleTimerState}>Start</button>;
+    } else {
+      return <span>{seconds}</span>;
+    }
+  };
+
   const testText = "the fox that jumped over the fence";
   useEffect(() => {
-
-
     const detectKeyDown = (e) => {
       console.log("Clicked key: ", e.key);
       if (e.key === testText[index] && e.key !== "Backspace") {
@@ -38,16 +55,23 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex h-screen w-full flex-col items-center justify-center">
-       
-       
-        <h1>{testText}</h1>
+        <Countdown
+          date={time}
+          renderer={renderer}
+          onComplete={() => setStarted(false)}
+        ></Countdown>
+        {started ? <h1>{testText}</h1> : <></>}
         <div className="flex">
-       
-            <p className={clsx({
+          <p
+            className={clsx({
               ["text-red-600"]: !correct,
               ["text-green-500"]: correct,
-            })}>{pressedKeys.map((i) => <>{i}</>)}</p>
-        
+            })}
+          >
+            {pressedKeys.map((i) => (
+              <>{i}</>
+            ))}
+          </p>
         </div>
       </main>
     </>
